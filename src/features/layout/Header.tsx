a37f79ffaee42846142e3e5f4ec7b99e452247d1
sebastien <sebastien.savan@gmail.com>
2023-12-26 07:47:44 +0100

    add color

--- src/features/layout/Header.tsx
+++ src/features/layout/Header.tsx
@@ -2,13 +2,16 @@ import React from "react"
 import { LoginButton } from "./LoginButton"
 import { getAuthSession } from "@/lib/auth"
 import { UserProfile } from "./UserProfile"
+import Link from "next/link"
 
 export const Header = async () => {
   const session = await getAuthSession()
   return (
     <header className='border-b border-b-accent'>
       <div className='container flex items-center max-w-lg mx-auto py-2 gap-1'>
-        <h2 className='text2xl font-bold mr-auto'>Green Book</h2>
+        <h2 className='text-2xl text-green-900 font-bold mr-auto'>
+          <Link href='/'>Green Book</Link>
+        </h2>
         {session?.user ? <UserProfile /> : <LoginButton />}
       </div>
     </header>
