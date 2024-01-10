502d5c319e26733cde3dbd0d329e9d0e3577381a
sebastien <sebastien.savan@gmail.com>
2024-01-10 20:29:28 +0100

    comment google provider

--- lib/auth.tsx
+++ lib/auth.tsx
@@ -12,10 +12,10 @@ export const authOptions: AuthOptions = {
       clientId: env.GITHUB_ID,
       clientSecret: env.GITHUB_SECRET,
     }),
-    GoogleProvider({
-      clientId: env.GOOGLE_ID,
-      clientSecret: env.GOOGLE_SECRET,
-    }),
+    // GoogleProvider({
+    //   clientId: env.GOOGLE_ID,
+    //   clientSecret: env.GOOGLE_SECRET,
+    // }),
   ],
   callbacks: {
     session({ session, user }) {
